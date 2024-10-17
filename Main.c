b1ccfbae34697ad9390ca007c6bcdec408fbc328
GabrielScachePrudencio <bielscache@gmail.com>
2024-10-17 00:52:52 -0300

    usando variavel global e fazendo numeros aleatorios de forma 10x melhor

--- Main.c
+++ Main.c
@@ -3,94 +3,66 @@
 #include <time.h>
 #include <string.h>
 
-#define tam 100
-#define tamInd 10
+#define tam 1000000
+#define tamInd 100
 
-void gerarNumAleatorio(int vetor[tam]);
-void mostrar(int vetor[tam]);
-void ordenadorEmOrdemCrescente(int vetor[tam]);
+void gerarNumAleatorio(int *vetor[tam]);
+void mostrar(int *vetor[tam]);
 
-void criarIndices(int vetor[tam], int vetorInd[tamInd]);
+void criarIndices(int *vetor[tam], int vetorInd[tamInd]);
 void mostrarIndices(int vetorInd[tamInd]);
+void buscadorDeNumeroPelosIndices(int *vetor[tam], int vetorInd[tamInd], int numProc);
 
+int *numGeradosAleatoriamente[tam];
 
 void main(){
     srand(time(NULL)); 
-    //rand() % 10;
-
-    int numGeradosAleatoriamente[tam];
 
     gerarNumAleatorio(numGeradosAleatoriamente);
     printf("\nNumeros gerados:\n");
     mostrar(numGeradosAleatoriamente);
 
-    ordenadorEmOrdemCrescente(numGeradosAleatoriamente);
-    printf("\nNumeros gerados agora ordenados:\n");
-    mostrar(numGeradosAleatoriamente);
-
-    
     int indicesDosNumerosAleatoriosOrdenados[tamInd];
-    int numProc = 99;
+    int numProc;
+    printf("\nDigite o numero a ser procurado: ");
+    scanf("%d", &numProc);
 
     criarIndices(numGeradosAleatoriamente, indicesDosNumerosAleatoriosOrdenados);
     printf("\nNumeros Indices:\n");
     mostrarIndices(indicesDosNumerosAleatoriosOrdenados);
     buscadorDeNumeroPelosIndices(numGeradosAleatoriamente, indicesDosNumerosAleatoriosOrdenados, numProc);
-
-
+    
 }
 
-void gerarNumAleatorio(int vetor[tam]){
-    int qtdd = 0;
-    for(int i = 0; i < tam; i++){
-        int achou = 0;
-        int numAlea = rand() % tam; // ele cria um num aleatorio
-
-        for(int j = 0; j < qtdd && achou == 0; j++){ //aqui ele verifica se ja existe
-            if(vetor[j] == numAlea){ 
-                achou = 1;// defini se existe ou nao
-                j = qtdd;//break
-            }
-        }
-
-        if(achou == 0){ // se nao existir ele passa pelo if
-            qtdd++; //o qtdd e uma forma de pelo menos no inicio tornar mais eficiente a busca pela duplicagem pq so ele so vai procurar ate quantos numeros ja foram criados nao o total
-            vetor[i] = numAlea; // passa o num aleatorio pelo indice i no vetor
-        } else { // caso ele tenha cido duplicado
-            i--; // ele retrocede e faz tudo de novo, nao duplicando
-        }      
+void gerarNumAleatorio(int *vetor[tam]){
+    
+    vetor[0] = rand() % 100;
+    for(int i = 1; i != tam; i++){
+        vetor[i] = vetor[i-1] + (rand() % 10);
+       
     }
 
-    printf("\n\n%d\n\n", qtdd);
+
 }
 
-void mostrar(int vetor[tam]){
+void mostrar(int *vetor[tam]){
+    int qtdd= 0;
     for(int i = 0; i < tam; i++){
         printf(" %d", vetor[i]);
-    }
-}
-
-void ordenadorEmOrdemCrescente(int vetor[tam]){
-    for(int i = 0; i < tam; i++){
-        for(int j = 0; j < tam; j++){
-            if(vetor[i] < vetor[j]){ // basciamente vai trocando o maior com menor deixar os menores na frente
-                int temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
-            }
-        }
+        qtdd++;
     }
 }
 
 
 
 // criando e mostrando os indices 
-void criarIndices(int vetor[tam], int vetorInd[tamInd]){
+void criarIndices(int *vetor[tam], int vetorInd[tamInd]){
     for(int i = 0; i < tamInd; i++){
         vetorInd[i] =  vetor[i * (tam/tamInd)]; 
         // basicamente como 10000/100 entao da 100 indices so que cada um desses indices é multiplicado por I entao é 0, 100, 200, 300, ate 9900
     }
 }
+
 
 void mostrarIndices(int vetorInd[tamInd]){
     for(int i = 0; i < tamInd; i++){
@@ -99,7 +71,7 @@ void mostrarIndices(int vetorInd[tamInd]){
 }
 
 
-void buscadorDeNumeroPelosIndices(int vetor[tam], int vetorInd[tamInd], int numProc){
+void buscadorDeNumeroPelosIndices(int *vetor[tam], int vetorInd[tamInd], int numProc){
     int achou = 0;
     for(int i = 0; i < tamInd; i++){ // ele vai ver pegar cada indice que e um numero 100 200
         if(numProc <= vetorInd[i]){ // e vai pegar o q for o ultimo menor 
@@ -119,9 +91,25 @@ void buscadorDeNumeroPelosIndices(int vetor[tam], int vetorInd[tamInd], int numP
     if(achou == 0) printf("\n\nO numero nao foi encontrado");
 }
 
+/*
+///metas 
+- buscadorDeNumeroPelosIndices nao ta funcionando
+- add mais vetores de indices mais cinco mais + -
+*/
 
 
-//NAO FUNCIONOU A buscadorDeNumeroPelosIndices
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
