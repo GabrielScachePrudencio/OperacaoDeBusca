fb99d19a138eb88df67486633559d825f5298c1c
Wesley-Martinss <wumartinss@gmail.com>
2024-11-02 11:44:40 -0300

    Atualiza o código do main

--- Main.c
+++ Main.c
@@ -9,7 +9,7 @@
 void gerarNumAleatorio(int *vetor);
 void criarIndices(int *vetor, int vetorInd[tamInd], int vetorInd2[tamInd]);
 void buscadorDeNumeroPelosIndices(int *vetor, int vetorInd[tamInd], int vetorInd2[tamInd], int numProc);
- // teste
+ 
 
 void main(){
     srand(time(NULL)); 
@@ -111,12 +111,7 @@ void buscadorDeNumeroPelosIndices(int *vetor, int vetorInd[tamInd], int vetorInd
 
 
 
-/*
-///metas 
-- fazer o readme
-- fazer relatorio
-- gravar o videp e postar no ytb
-*/
+
 
 
 
